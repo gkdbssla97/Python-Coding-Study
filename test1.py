c377e8e1ba7c68997a4816b0f6997ce01a643f45
Hayoon <55674664+gkdbssla97@users.noreply.github.com>
2021-09-11 19:11:35 +0900

    큰수의법칙 #1

---
+++ test1.py
@@ -0,0 +1,23 @@
+N, M, K = map(int, input().split())
+
+memo = []
+data = list(map(int,input().split()))
+data.sort(reverse = True)
+
+cnt = 0
+sum = 0
+
+while True :
+    sum += data[0]
+    cnt += 1
+    M -= 1
+    #print(sum)
+    if cnt == K :
+        sum += data[1]
+        M -= 1
+        cnt = 0
+    if M == 0 :
+        break
+
+print(sum)
+print(data)
